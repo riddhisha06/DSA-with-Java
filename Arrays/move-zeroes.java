1bffd0007fd9ce0246509d6c07dabaaf3f77e783
Riddhisha Srivastava <161882496+riddhisha06@users.noreply.github.com>
2026-03-12 21:07:53 +0530

    Add MoveZeroes class to move zeroes in an array

---
+++ Arrays/move-zeroes.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+public class MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        System.out.println("Original Array: " + Arrays.toString(nums));
+        moveZeroes(nums);
+        System.out.println("After Moving Zeroes: " + Arrays.toString(nums));
+    }
+}
