ddcbb0c7bedbce69449b80181cd36971c00c45e7
Riddhisha Srivastava <161882496+riddhisha06@users.noreply.github.com>
2026-03-15 22:32:41 +0530

    Implement ValidAnagram class with anagram check

---
+++ Strings/valid-anagram.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int num : count) {
+            if (num != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "anagram";
+        String t1 = "nagaram";
+
+        String s2 = "rat";
+        String t2 = "car";
+
+        System.out.println("Example 1: " + isAnagram(s1, t1)); // true
+        System.out.println("Example 2: " + isAnagram(s2, t2)); // false
+    }
+}
