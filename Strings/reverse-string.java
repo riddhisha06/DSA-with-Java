43938ace4cf5defe55e32a731f505662a86cbf90
Riddhisha Srivastava <161882496+riddhisha06@users.noreply.github.com>
2026-03-16 23:55:43 +0530

    Implement reverse string functionality in Java

---
+++ Strings/reverse-string.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+public class ReverseString {
+    public static void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] s = {'c','o','m','p','u','t','e','r'};
+        reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+}
