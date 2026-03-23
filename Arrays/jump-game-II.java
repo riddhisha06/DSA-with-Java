f8a34311891554bba2d760046257f79f99169098
Riddhisha Srivastava <161882496+riddhisha06@users.noreply.github.com>
2026-03-23 23:23:26 +0530

    Implement jump game II solution in Java

---
+++ Arrays/jump-game-II.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int currEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == currEnd) {
+                jumps++;
+                currEnd = farthest;
+            }
+        }
+        return jumps;
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(obj.jump(nums));
+    }
+}
