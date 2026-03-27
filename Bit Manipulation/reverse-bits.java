98ccfc8894ddf98ef64a785d2437ce06edd03e2e
Riddhisha Srivastava <161882496+riddhisha06@users.noreply.github.com>
2026-03-27 16:44:02 +0530

    Create reverse-bits.java

---
+++ Bit Manipulation/reverse-bits.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            result |= (n & 1);
+            n >>= 1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n = 43261596;
+        int reversed = reverseBits(n);
+        System.out.println("Original: " + n);
+        System.out.println("Reversed Bits Result: " + reversed);
+    }
+}
