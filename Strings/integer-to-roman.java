daf696e00e00e19490555c1118f9df8ea81cbb2a
Riddhisha Srivastava <161882496+riddhisha06@users.noreply.github.com>
2026-03-30 22:42:07 +0530

    Implement integer to Roman numeral conversion

---
+++ Strings/integer-to-roman.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] values = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
+
+        String[] symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i < values.length; i++) {
+            while(num >= values[i]) {
+                result.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int num = 1994;
+        String roman = sol.intToRoman(num);
+
+        System.out.println("Integer: " + num);
+        System.out.println("Roman: " + roman);
+    }
+}
