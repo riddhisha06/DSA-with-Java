051067cb94873ed844f3ccd42059a5dfbc598b40
Riddhisha Srivastava <161882496+riddhisha06@users.noreply.github.com>
2026-04-30 22:45:30 +0530

    Add deleteNode and printList methods for linked list
    Implemented deleteNode method to remove a node from the linked list and added a printList method to display the list.

--- Linked List/delete-node-in-a-linked-list.java
+++ Linked List/delete-node-in-a-linked-list.java
@@ -1,1 +1,37 @@
-code
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Main {
+    public static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+        System.out.println("Original List:");
+        printList(head);
+        ListNode nodeToDelete = head.next;
+        deleteNode(nodeToDelete);
+        System.out.println("After Deletion:");
+        printList(head);
+    }
+}
